c2df8868910b2ffc7bc5590aaa86cb206e001d8e
liravarel <105433878+liravarel@users.noreply.github.com>
2022-06-14 15:45:26 -0300

    Determine oque será mostrado

---
+++ Questao5.c
@@ -0,0 +1,14 @@
+Determine o que será mostrado pelo seguinte programa (compile-e, execute-o e verifique se foram
+obtidas as respostas esperadas).
+
+Na questão dá 5 vetores que são: (0,1,2,3 e 4). obs: Na questão já começa com o vetor 0 incluso.
+e também os valores contido em cada vetores que são: (1.1,2.2,3.3,4.4,5.5)
+e dá um printf ( “ contador/valor/valor/endereço/endereço”); como quer ser mostrado no compilador.
+
+resultado:
+contador/valor/valor/endereço/endereço
+i= 0 vet[0]= 1.1* (f+0)=1.1&vet[0]=F1711EC0(f+0)=F1711EC0
+i= 1 vet[1]= 2.2* (f+1)=2.2&vet[1]=F1711EC4(f+1)=F1711EC4
+.
+.
+i= 4 vet[4]= 4.4* (f+1)=4.4&vet[4]=F1711EC4(f+4)=F1711ED0
