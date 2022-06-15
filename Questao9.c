248a180c7c7bce3ea275662cb50905d4060fd098
liravarel <105433878+liravarel@users.noreply.github.com>
2022-06-15 09:46:23 -0300

    Quais serão os valores de x+1, x+2 e x+3

---
+++ Questao9.c
@@ -0,0 +1,6 @@
+Quais serão os valores de x+1, x+2 e x+3
+se:
+◦ x for declarado como char? // X+1=4093, e X+2=4094, e X+3=4095
+◦ x for declarado como int? // X+1=4094, e X+2=4096, e X+3=4098
+◦ x for declarado como float? // x+1=4096, e X+2=409A, e X+3=409E
+◦ x for declarado como double? //X+1=409A, e X+2=40A2, e X+3=40AA
