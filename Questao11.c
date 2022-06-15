cf2cadbc58abec9672e973213dfcf46b31945e4f
liravarel <105433878+liravarel@users.noreply.github.com>
2022-06-15 09:56:47 -0300

    As seguintes declarações foram realizadas

---
+++ Questao11.c
@@ -0,0 +1,11 @@
+Suponha que as seguintes declarações tenham sido realizadas:
+
+Aloha [2]= value; Válido.
+Scanf (“&f”, aloha); Inválida.
+porque não tem []
+Aloha= value; inválido
+Printf(“%f”, aloha); Inválida.
+Coisas[4] [4]= aloha[3]; Válido
+Coisas [5]= aloha; Válido
+Pf= value; ( se for um ponteiro, é válido sim, pois irá retornar value, ou aloha)
+Pf=aloha; ( se for um ponteiro, é válido sim, pois irá retornar value, ou aloha)
