2089fd00621f022691d2c55c05bf21a8c68a7881
liravarel <105433878+liravarel@users.noreply.github.com>
2022-06-16 11:14:44 -0300

    Update Questao7.c

--- Questao7.c
+++ Questao7.c
@@ -1,7 +1,9 @@
+ Quais são válidas?
+
 p = mat + 1;	
 p = mat++;	
 p = ++mat;	
 x = (*mat)++; 	
 
-//Resultado: apenas p=mat+1; e x=(*mat)++; estão corretas, porque não pode incrementar
+//Resultado: apenas p=mat+1; e x=(*mat)++; estão corretas, porque não podem incrementar
 valor do tipo int ao ponteiro.
