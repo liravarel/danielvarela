f2583d85f4ded83d3870fa7421d634a6fa52080d
liravarel <105433878+liravarel@users.noreply.github.com>
2022-06-16 11:26:22 -0300

    Update Questao8.c

--- Questao8.c
+++ Questao8.c
@@ -1,4 +1,5 @@
 Exemplo 1:
+
 int main(){ int vet[] = {4,9,13}; int i; for(i=0;i<3;i++){ printf("%d ",*(vet+i)); } }
 Resultado:
 4,9 e 13
@@ -11,6 +12,6 @@ int main(){ int vet[] = {4,9,13}; int i; for(i=0;i<3;i++){ printf("%X ",vet+i); 
 Resultado:
 1F745B50 1F745B54 1F745B58
 
-Na questão diz que i=0; e i<3; ou seja o compilador entende que i será maior que 3, então compilou valores crescentes em 4 unidades, como: 0,4 e 8.
+Na questão diz que i=0; e i<3; ou seja o compilador entende que i será maior que 3, então compilou valores crescentes em 4 unidades, como: 0, 
 
 obs: O compilador começou no número 0 e seguiu uma ordem de 0,4 e 8
